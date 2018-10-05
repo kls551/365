a742723cba46c2bff06d6e26b46964cce9e5a21a
Kaitlin Bleich <kbleich@calpoly.edu>
2018-10-05 15:30:52 -0700

    add

---
+++ MARATHON-setup.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Marathon (
+   Place int,
+   Times Time,
+   Pace Time,
+   GroupPlace int,
+   Group CHAR(6),
+   Age int,
+   Sex Char(1),
+   BIBNumber int,
+   FirstName CHAR(30),
+   LastName CHAR(30),
+   Town CHAR(40),
+   State CHAR(2)
+)

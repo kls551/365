7bb997bae55bfffe978a925ad9a7b85040e48b70
kls551 <32246763+kls551@users.noreply.github.com>
2018-10-05 15:48:08 -0700

    CSU imple

---
+++ CSU/CSU-test.sql
@@ -0,0 +1,20 @@
+SELECT * FROM Campuses;
+SELECT COUNT(*) FROM Campuses;
+
+SELECT * FROM DisEnrollments;
+SELECT COUNT(*) FROM DisEnrollments;
+
+SELECT * FROM degrees;
+SELECT COUNT(*) FROM degrees;
+
+SELECT * FROM disciplines;
+SELECT COUNT(*) FROM disciplines;
+
+SELECT * FROM enrollments;
+SELECT COUNT(*) FROM enrollments;
+
+SELECT * FROM faculty;
+SELECT COUNT(*) FROM faculty;
+
+SELECT * FROM fees;
+SELECT COUNT(*) FROM fees;

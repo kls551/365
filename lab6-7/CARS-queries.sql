1af361a3602efec9b4dada52b4621863505ee5b9
Kaitlin Bleich <kbleich@calpoly.edu>
2018-11-29 20:35:02 -0800

    All but 5

--- lab6-7/CARS-queries.sql
+++ lab6-7/CARS-queries.sql
@@ -61,6 +61,7 @@ WHERE
 
 -- Q3
 -- report full name and year of cars with best gas mileage
+-- TODO only one car returned??
 SELECT
    CN.Description
    , CD.YearMade
@@ -83,47 +84,107 @@ WHERE
 
 -- Q4
 -- among best MPG cars report best acceleration (full name and year)
+SELECT maxes.max, cars.Description, cars.yearmade
+FROM
+   (SELECT
+   MAX(subq.Accelerate) as max
+   FROM
+      (SELECT CN.Description, CD.YearMade, CD.Accelerate
+      FROM countries CTR, carMakers CM, modelList ML, carNames CN, carsData CD
+      WHERE CM.Country = CTR.Id AND CM.Id = ML.Maker AND ML.model = CN.Model AND CN.Id = CD.Id AND
+            CD.MPG = (SELECT MAX(CD.MPG) FROM carsData CD)) as subq) as maxes,
+      (SELECT CN.Description, CD.YearMade, CD.Accelerate
+      FROM countries CTR, carMakers CM, modelList ML, carNames CN, carsData CD
+      WHERE CM.Country = CTR.Id AND CM.Id = ML.Maker AND ML.model = CN.Model AND CN.Id = CD.Id AND
+            CD.MPG = (SELECT MAX(CD.MPG) FROM carsData CD)) as cars
+      ;
 
 -- Q5
 -- report automakers whose models were on average the heaviest 
 -- report year, automaker, number of models that year, avg acceleration
 -- exclude anyone with only one car that year
--- SELECT SUBQ.YearMade, OCM.FullName, MAX(SUBQ.avg)
--- FROM 
---    carMakers OCM
---    , (SELECT
---    CD.YearMade
---    , CM.FullName
---    , AVG(CD.Weight) as avg
---    FROM
---    countries CTR
---    , carMakers CM
---    , modelList ML
---    , carNames CN
---    , carsData CD
---    WHERE
---    -- connect countries -> carMakers
---    CM.Country = CTR.Id AND
---    -- connect carMakers -> carNames
---    CM.Id = ML.Maker AND
---    ML.model = CN.Model AND
---    -- connect carNames -> carsData
---    CN.Id = CD.Id 
---    -- other criteria
---    GROUP BY
---    CD.YearMade
---    , CM.FullName) as SUBQ
--- WHERE
---    OCM.FullName = SUBQ.FullName
--- GROUP BY
---    SUBQ.YearMade;
+-- TODO ask Prof about cutting this down to only makers with > 1 car, and how to report how many
+SELECT
+   CM1.FullName
+   , CD1.YearMade
+   , COUNT(DISTINCT CN1.Description)
+   , TRUNCATE(AVG(CD1.Accelerate), 2)
+FROM
+    carMakers CM1
+    , modelList ML1
+    , carNames CN1
+    , carsData CD1
+WHERE
+    -- connect carMakers -> carNames
+    CM1.Id = ML1.Maker AND
+    ML1.model = CN1.Model AND
+    -- connect carNames -> carsData
+    CN1.Id = CD1.Id
+GROUP BY
+   CD1.YearMade, CM1.FullName
+HAVING AVG(CD1.Weight) >= ALL(
+   SELECT subsubq.avg 
+   FROM 
+      (SELECT ML.Maker, CD.YearMade, AVG(CD.Weight) as avg
+       FROM carsData CD, carNames CN, modelList ML
+       WHERE ML.model = CN.Model AND
+             CN.Id = CD.Id 
+       GROUP  BY ML.maker, CD.YearMade) as subsubq
+   WHERE subsubq.YearMade = CD1.YearMade); 
 
 -- Q6
 -- report diff between most efficient 8cyl and least efficient 4banger
 SELECT 
    (SELECT MAX(CD.MPG) FROM carsData CD WHERE CD.Cylinders = 8) -
-   (SELECT MIN(CD.MPG) FROM carsData CD WHERE CD.Cylinders = 4) as SUBQ
+   (SELECT MIN(CD.MPG) FROM carsData CD WHERE CD.Cylinders = 4) as SUBQ;
 
 -- Q7
 -- For years b/w 1972 & 1976 find out if US automakers or others made more
 -- cars. Report either US or 'The Rest of The World'
+SELECT
+   usa.yearMAde, IF(usa.count > nusa.count, 'US', 'The rest of the world')
+FROM
+(SELECT
+   CD.YearMade
+   ,COUNT(CN.Id) count
+FROM
+   countries CTR
+   , carMakers CM
+   , modelList ML
+   , carNames CN
+   , carsData CD
+WHERE
+   -- connect countries -> carMakers
+   CM.Country = CTR.Id AND
+   -- connect carMakers -> carNames
+   CM.Id = ML.Maker AND
+   ML.model = CN.Model AND
+   -- connect carNames -> carsData
+   CN.Id = CD.Id AND
+   -- other criteria 
+   CTR.Name = 'usa' AND CD.YearMade BETWEEN 1972 AND 1976
+GROUP BY
+   CD.YearMade) as usa,
+(SELECT
+   CD.YearMade
+   ,COUNT(CN.Id) count
+FROM
+   countries CTR
+   , carMakers CM
+   , modelList ML
+   , carNames CN
+   , carsData CD
+WHERE
+   -- connect countries -> carMakers
+   CM.Country = CTR.Id AND
+   -- connect carMakers -> carNames
+   CM.Id = ML.Maker AND
+   ML.model = CN.Model AND
+   -- connect carNames -> carsData
+   CN.Id = CD.Id AND
+   -- other criteria 
+   CTR.Name <> 'usa' AND CD.YearMade BETWEEN 1972 AND 1976
+GROUP BY
+   CD.YearMade) as nusa
+WHERE
+   usa.YEarMAde = nusa.YEarMade;
